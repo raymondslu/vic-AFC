b74c0155124ee4960f60832524b3eb886d95c4be
vic-cheung <victoria.cheung@ucsf.edu>
2017-07-28 09:13:32 -0700

    Add files via upload

---
+++ AFC_poketest.ino
@@ -0,0 +1,32 @@
+int middleLED = 9;
+int leftLED = 10;
+int rightLED = 11;
+
+int middleDetector = A0;
+int leftDetector = A1;
+int rightDetector = A2;
+
+void setup() {
+pinMode(middleLED,OUTPUT);
+pinMode(leftLED,OUTPUT);
+pinMode(rightLED,OUTPUT);
+
+pinMode(middleDetector,INPUT);
+pinMode(leftDetector,INPUT);
+pinMode(rightDetector,INPUT);
+}
+
+void loop(){
+  if (digitalRead(middleDetector)==HIGH)
+  {
+     digitalWrite(middleLED,HIGH);
+     digitalWrite(leftLED,HIGH);
+     digitalWrite(rightLED,HIGH);
+  }
+  if (digitalRead(middleDetector)==LOW)
+  {
+     digitalWrite(middleLED,LOW);
+     digitalWrite(leftLED,LOW);
+     digitalWrite(rightLED,LOW);
+  }
+}
